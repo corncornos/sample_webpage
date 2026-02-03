4216516b55d55f37138e0e13ee7dbb98eb756b82
corncornos <167315630+corncornos@users.noreply.github.com>
2026-02-03 14:11:52 +0800

    Update app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://car-inventory-backend-sample-webpage-ce14.onrender.com';;
 let token = localStorage.getItem('token');
 let currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}');
 let vehicleModal, saleModal, userModal;
